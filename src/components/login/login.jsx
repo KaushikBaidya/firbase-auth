e0073553b9da854c7ca14223359e0a8b2a79d11e
KaushikBaidya <47146123+KaushikBaidya@users.noreply.github.com>
2022-03-15 11:55:07 +0600

    signup

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './login.module.css'
-import InputControl from '../InputControll/InputControl'
+import InputControl from '../InputControl/InputControl'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
@@ -8,6 +9,15 @@ const Login = () => {
       <div className={styles.innerBox}>
         <h1 className={styles.heading}>Login</h1>
         <InputControl label="Email" placeholder="Enter email address" />
+        <InputControl label="Password" placeholder="Enter email address" />
+
+        <div className={styles.footer}>
+          <button>Login</button>
+          <p>Don't have an account</p>
+          <span>
+            <Link to="/signup">Sign Up</Link>
+          </span>
+        </div>
       </div>
     </div>
   )
--- src/components/signUp/signUp.jsx
+++ src/components/signUp/signUp.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from './signUp.module.css'
+import InputControl from '../InputControl/InputControl'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
-  return <div>SignUp</div>
+  const [values, SetValues] = useState({
+    name: '',
+    email: '',
+    pass: '',
+  })
+  return (
+    <div className={styles.container}>
+      <div className={styles.innerBox}>
+        <h1 className={styles.heading}>Sign Up</h1>
+        <InputControl
+          label="Name"
+          placeholder="Enter Your Name"
+          onChange={(e) =>
+            SetValues((prev) => ({ ...prev, name: e.target.value }))
+          }
+        />
+        <InputControl label="Email" placeholder="Enter email address" />
+        <InputControl label="Password" placeholder="Enter Password" />
+
+        <div className={styles.footer}>
+          <button>Login</button>
+          <p>Already have an account</p>
+          <span>
+            <Link to="/login">Login</Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default SignUp
