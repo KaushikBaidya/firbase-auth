70d0607cc680f4570c57a0cabe631c33f82ef649
KaushikBaidya <47146123+KaushikBaidya@users.noreply.github.com>
2022-03-15 20:05:08 +0600

    styling

--- src/components/signUp/signUp.jsx
+++ src/components/signUp/signUp.jsx
@@ -1,14 +1,42 @@
 import React, { useState } from 'react'
 import styles from './signUp.module.css'
 import InputControl from '../InputControl/InputControl'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { auth } from '../../firebase'
 
 const SignUp = () => {
-  const [values, SetValues] = useState({
+  const navigate = useNavigate()
+  const [errorMsg, setErrorMsg] = useState('')
+  const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false)
+  const [values, setValues] = useState({
     name: '',
     email: '',
     pass: '',
   })
+
+  const handleSubmission = () => {
+    if (!values.name || !values.email || !values.pass) {
+      setErrorMsg('Fill all fields')
+      return
+    }
+    setErrorMsg('')
+
+    setSubmitButtonDisabled(true)
+
+    createUserWithEmailAndPassword(auth, values.email, values.pass)
+      .then(async (res) => {
+        setSubmitButtonDisabled(false)
+        const user = res.user
+        await updateProfile(user, { displayName: values.name })
+        navigate('/')
+      })
+      .catch((err) => {
+        setSubmitButtonDisabled(false)
+        setErrorMsg(err.message)
+      })
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.innerBox}>
@@ -17,14 +45,29 @@ const SignUp = () => {
           label="Name"
           placeholder="Enter Your Name"
           onChange={(e) =>
-            SetValues((prev) => ({ ...prev, name: e.target.value }))
+            setValues((prev) => ({ ...prev, name: e.target.value }))
           }
         />
-        <InputControl label="Email" placeholder="Enter email address" />
-        <InputControl label="Password" placeholder="Enter Password" />
+        <InputControl
+          label="Email"
+          placeholder="Enter email address"
+          onChange={(e) =>
+            setValues((prev) => ({ ...prev, email: e.target.value }))
+          }
+        />
+        <InputControl
+          label="Password"
+          placeholder="Enter Password"
+          onChange={(e) =>
+            setValues((prev) => ({ ...prev, pass: e.target.value }))
+          }
+        />
 
         <div className={styles.footer}>
-          <button>Login</button>
+          <b className={styles.error}>{errorMsg}</b>
+          <button onClick={handleSubmission} disabled={submitButtonDisabled}>
+            Sign up
+          </button>
           <p>Already have an account</p>
           <span>
             <Link to="/login">Login</Link>
