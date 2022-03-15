5bc16a25a2e045d3802918d0151821ff64352bbd
KaushikBaidya <47146123+KaushikBaidya@users.noreply.github.com>
2022-03-15 11:32:22 +0600

    login updated

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import styles from './login.module.css'
+import InputControl from '../InputControll/InputControl'
 
 const Login = () => {
-  return <div>Login</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.innerBox}>
+        <h1 className={styles.heading}>Login</h1>
+        <InputControl label="Email" placeholder="Enter email address" />
+      </div>
+    </div>
+  )
 }
 
 export default Login
